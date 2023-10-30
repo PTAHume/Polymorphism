b4bb6118d24163682c7cd4ba2934df2f8b517731
Patrick Hume <patricktahume@outlook.com>
2023-10-30 15:10:18 +0000

    Adding abstract class

--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -1,29 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PolymorphismC;
 
 namespace Polymorphism
 {
-    class Program
+    internal class Program
     {
-
         // Create a base class Car with two properties HP and Color
         // Create a Constructor setting those two properties
         // Create a Method called ShowDetails() which shows the HP and Color of the car on the console
-        // Create a Repair Method which writes "Car was repaired!" onto the console 
+        // Create a Repair Method which writes "Car was repaired!" onto the console
         // Create two deriving classes, BMW and Audi, which have their own constructor and have an aditional property
         // called Model. Also a private member called brand. Brand should be different in each of the two classes.
         // Create the two methods ShowDetails() and Repair in them as well. Adjust those methods accordingly.
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-
             // a car can be a BMW, an Audi, a Porsche etc.
             // Polymorphism at work #1: an Audi,  BMW, Porsche
             // can all be used whereever a Car is expected. No cast is
-            // required because an implicit conversion exists from a derived 
+            // required because an implicit conversion exists from a derived
             // class to its base class.
             var cars = new List<Car>
             {
@@ -61,6 +61,15 @@ namespace Polymorphism
 
             M3 myM3 = new M3(260, "red", "M3 Super Turbo");
             myM3.Repair();
+
+            Shape[] shapes = { new Sphere(4) , new Cube(3) };
+
+            foreach(Shape shape in shapes)
+            {
+                shape.GetInfo();
+                Console.WriteLine("{0} has a volume of {1}", shape.Name, shape.Volume());
+            }
+
             Console.ReadKey();
         }
     }
