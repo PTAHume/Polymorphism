2b78e3fbac636c3ff74eab7110e6adc842a72afa
Patrick Hume <patricktahume@outlook.com>
2023-11-01 17:10:05 +0000

    notes

--- Polymorphism/Game/GameEventManager.cs
+++ Polymorphism/Game/GameEventManager.cs
@@ -6,7 +6,12 @@ namespace PolymorphismC
     {
         public delegate void GameEvent();
 
-        public  static event GameEvent OnGameStart, OnGameOver;
+        /*Events are forced to behave like a list (+=/-=) 
+          foo += mydelgate or foo -= mydelgate is ok but foo = mydelgate is not ok
+            Delgate allows signing of = so foo = mydelgate is now ok as is the above 
+            but deligate will be overidden whenever set so only set the once
+        */
+        public static event GameEvent OnGameStart, OnGameOver;
 
         public static void TriggerGameStart()
         {
